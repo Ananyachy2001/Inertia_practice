0bee81224e09142c71052eb68fb05237f7b8da1b
Ananya <Ananyachy2001@gmail.com>
2024-02-22 14:46:03 +0600

    data passing

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import { Head } from '@inertiajs/vue3'
+
+defineProps({
+    name: String,
+    frameworks: Array,
+
+})
+
+</script>
+
+<template>
+    <h1>Welcome</h1>
+    <h2> {{ name }} </h2>
+    <p>We will use this seris</p>
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+</template>
